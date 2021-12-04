ae0fdcbeac0aba9459c56037fd4ff179f5e920db
katsumi-watanabe <ktm981124@gmail.com>
2021-12-04 15:43:12 +0900

    ループ

--- 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
+++ 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
@@ -1,5 +1,10 @@
 const arry = [1, 2, 3, 4, 5, 6];
 
-for (let i = 0; i < arry.length; i++) {
-    console.log(i);
+// for (let i in arry) {
+//     console.log(arry[i]);
+// }
+
+for (let v of arry) {
+    // let v = arry[1];
+    console.log(v);
 }
