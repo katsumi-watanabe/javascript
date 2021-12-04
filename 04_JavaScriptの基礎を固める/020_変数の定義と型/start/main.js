c50e3e5514c4e4ae98d628b80d5de1351cd1126f
katsumi-watanabe <ktm981124@gmail.com>
2021-12-04 09:48:52 +0900

    JavaScript基礎

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,11 @@
+// var, let, const
+
+let name = 'Tom';
+name = 'Tim';
+console.log('hello' + name);
+
+let variable = 'str';
+variable = 12;
+variable = false;
+console.log(typeof variable);
+variable = undefined;
