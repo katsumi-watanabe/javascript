1de0803fcaf9247eee6addf3c9ce59381baacc08
katsumi-watanabe <ktm981124@gmail.com>
2021-12-04 17:34:14 +0900

    アロー関数

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,26 @@
+// function hello(name = 'Tom') {
+//   console.log('hello' + name);
+// }
+
+// function hello(name = 'Tom') {
+//   console.log('hello' + name);
+//   console.log('hello' + name);
+// }
+
+
+const hello = (name, age) => 40;
+
+const arry = [1, 2, 3, 4, 5, 6];
+arry.forEach(value => console.log(value));
+
+
+// console.log(hello());
+
+// const hello = (name = 'Tom') => console.log('hello' + name);
+
+// const hello = (name, age) => console.log('hello' + name);
+
+// const hello = name => console.log('hello' + name);
+
+// hello('Code Mafia', 10);
+// hello('Code Mafia 2', 20);
