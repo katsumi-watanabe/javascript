1e771eec1e122e251e7974e1c42596c74ba16d19
katsumi-watanabe <ktm981124@gmail.com>
2021-12-08 23:23:04 +0900

    Class

--- 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
+++ 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const el = document.querySelector('.animate-title');
-    const el2 = document.querySelector('.animate-title-2');
-    const str = el.innerHTML.trim().split("");
-    const str2 = el2.innerHTML.trim().split("");
+
+const ta = new TextAnimation('.animate-title');
+const ta2 = new TextAnimation('.animate-title-2');
+    // const el = document.querySelector('.animate-title');
+    // const el2 = document.querySelector('.animate-title-2');
+    
+    // const str2 = el2.innerHTML.trim().split("");
     
     // let concatStr = '';
 
@@ -11,22 +14,28 @@ document.addEventListener('DOMContentLoaded', function () {
     //     concatStr += `<span class="char">${c}</span>`;
     // }
 
-    el.innerHTML = str.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-    }, "");
-    el2.innerHTML = str2.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-    }, "");
+    // el.innerHTML = str.reduce((acc, curr) => {
+    //     curr = curr.replace(/\s+/, '&nbsp;');
+    //     return `${acc}<span class="char">${curr}</span>`;
+    // }, "");
+    // el2.innerHTML = str2.reduce((acc, curr) => {
+    //     curr = curr.replace(/\s+/, '&nbsp;');
+    //     return `${acc}<span class="char">${curr}</span>`;
+    // }, "");
 });
 
 class TextAnimation {
     constructor(el) {
-        this.el = el;
-        alert(el);
+        this.el = document.querySelector(el);
+        this.chars = this.el.innerHTML.trim().split("");
+        this.el.innerHTML = this._splitText();
+    }
+
+    _splitText() {
+        return this.chars.reduce((acc, curr) => {
+            curr = curr.replace(/\s+/, '&nbsp;');
+            return `${acc}<span class="char">${curr}</span>`;
+        }, "");
     }
 }
-
-const ta = new TextAnimation('hello world');
-alert(ta.el);
+// alert(ta.el);
