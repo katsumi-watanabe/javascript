27e9d4a339e9c376b1cd50f91f568363e617669a
katsumi-watanabe <ktm981124@gmail.com>
2021-12-09 10:40:27 +0900

    Class

--- 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
+++ 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
@@ -1,27 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-const ta = new TextAnimation('.animate-title');
-const ta2 = new TextAnimation('.animate-title-2');
-    // const el = document.querySelector('.animate-title');
-    // const el2 = document.querySelector('.animate-title-2');
-    
-    // const str2 = el2.innerHTML.trim().split("");
-    
-    // let concatStr = '';
-
-    // for(let c of str) {
-    //     c = c.replace(/\s+/, '&nbsp;');
-    //     concatStr += `<span class="char">${c}</span>`;
-    // }
-
-    // el.innerHTML = str.reduce((acc, curr) => {
-    //     curr = curr.replace(/\s+/, '&nbsp;');
-    //     return `${acc}<span class="char">${curr}</span>`;
-    // }, "");
-    // el2.innerHTML = str2.reduce((acc, curr) => {
-    //     curr = curr.replace(/\s+/, '&nbsp;');
-    //     return `${acc}<span class="char">${curr}</span>`;
-    // }, "");
+    const ta = new TextAnimation('.animate-title');
+    const ta2 = new TextAnimation('.animate-title-2');
+    setTimeout(() => {
+        ta.animate();
+        ta2.animate();
+    }, 1000);
 });
 
 class TextAnimation {
@@ -36,6 +20,9 @@ class TextAnimation {
             curr = curr.replace(/\s+/, '&nbsp;');
             return `${acc}<span class="char">${curr}</span>`;
         }, "");
+    }
+    animate() {
+        this.el.classList.toggle('inview');
     }
 }
 // alert(ta.el);
