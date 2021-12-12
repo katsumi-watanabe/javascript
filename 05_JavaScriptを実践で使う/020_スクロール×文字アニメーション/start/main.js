d40b8015f9def9566559b18d74c6458fbd1140e1
katsumi-watanabe <ktm981124@gmail.com>
2021-12-12 17:37:33 +0900

    スクロール監視クラス

--- 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
@@ -1,31 +1,63 @@
 document.addEventListener('DOMContentLoaded', function () {
     
-    
+    const cb = function (el, isIntersecting) {
+        if (isIntersecting) {
+            const ta = new TextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    const so = new ScrollObserver('.animate-title', cb);
+
     // text-animation.jsに以下のコードをカット＆ペースト
     // してファイル分割をしましょう。
-    const els = document.querySelectorAll('.animate-title');
-    const cb = function (entries, observer) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const ta = new TextAnimation(entry.target);
-                ta.animate();
-                observer.unobserve(entry.target);
-                // console.log('inview');
-                // entry.target.classList.add('inview');
-                // observer.unobserve(entry.target);
-            } else {
-                // console.log('out view');
-                entry.target.classList.remove('inview');
-            }
-        });
-        // alert('intersecting');
-    }
-    const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0
-    }
     
-    const io = new IntersectionObserver(cb, options);
-    els.forEach(el => io.observe(el));
+    // const cb = function (entries, observer) {
+    //     entries.forEach(entry => {
+    //         if (entry.isIntersecting) {
+    //             // const ta = new TextAnimation(entry.target);
+    //             // ta.animate();
+    //             observer.unobserve(entry.target);
+    //             // console.log('inview');
+    //             // entry.target.classList.add('inview');
+    //             // observer.unobserve(entry.target);
+    //         } else {
+    //             // console.log('out view');
+    //             entry.target.classList.remove('inview');
+    //         }
+    //     });
+    //     // alert('intersecting');
+    // }
+    
+    
 });
+
+class ScrollObserver{
+    constructor(els, cb, options) {
+        this.els = document.querySelectorAll(els);
+        const defaultOptions = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0
+        };
+        this.cb = cb;
+        this.options = Object.assign(defaultOptions, options);
+        this._init();
+    }
+    _init() {
+        const callback = function (entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+
+                    this.cb(entry.target, true);
+                    observer.unobserve(entry.target);
+                } else {
+                    this.cb(entry.target, false);
+                }
+            });
+        };
+        const io = new IntersectionObserver(callback.bind(this), this.options);
+        this.els.forEach(el => io.observe(el));
+    }
+}
+
