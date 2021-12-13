e3db211d9df6711ed0d7edb9c6f344690c200496
katsumi-watanabe <ktm981124@gmail.com>
2021-12-13 19:00:15 +0900

    スクロール監視クラス

--- 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
@@ -38,26 +38,34 @@ class ScrollObserver{
         const defaultOptions = {
             root: null,
             rootMargin: "0px",
-            threshold: 0
+            threshold: 0,
+            once: true
         };
         this.cb = cb;
         this.options = Object.assign(defaultOptions, options);
+        this.once = this.options.once;
         this._init();
     }
     _init() {
         const callback = function (entries, observer) {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-
                     this.cb(entry.target, true);
+                    if (this.once) {
+                        observer.unobserve(entry.target);
+                    }
                     observer.unobserve(entry.target);
                 } else {
                     this.cb(entry.target, false);
                 }
             });
         };
-        const io = new IntersectionObserver(callback.bind(this), this.options);
-        this.els.forEach(el => io.observe(el));
+        this.io = new IntersectionObserver(callback.bind(this), this.options);
+        this.els.forEach(el => this.io.observe(el));
+    }
+
+    destroy() {
+        this.io.disconnect();
     }
 }
 
