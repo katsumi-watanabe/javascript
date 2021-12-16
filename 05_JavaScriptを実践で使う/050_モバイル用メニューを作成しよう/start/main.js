738a1861ca844142943cc6aac1626be328478c9d
katsumi-watanabe <ktm981124@gmail.com>
2021-12-16 11:54:40 +0900

    スマホメニュー

--- 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -1,7 +1,8 @@
 class MobileMenu{
   constructor() {
     this.DOM = {};
-    this.DOM.btn = document.querySelector('.mobile-menu-btn');
+    this.DOM.btn = document.querySelector('.mobile-menu__btn');
+    this.DOM.cover = document.querySelector('.mobile-menu__cover');
     this.DOM.container = document.querySelector('#global-container');
     this.eventType = this._getEventType();
     this._addEvent();
@@ -23,6 +24,7 @@ class MobileMenu{
 
   _addEvent() {
     this.DOM.btn.addEventListener('click',this._toggle.bind(this));
+    this.DOM.cover.addEventListener('click',this._toggle.bind(this));
   }
 }
 
