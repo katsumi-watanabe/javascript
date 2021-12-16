b13d3bb75d48991762cf9111b7f5d74ce877099e
katsumi-watanabe <ktm981124@gmail.com>
2021-12-16 23:55:51 +0900

    ヒーロースライダー

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,6 +1,4 @@
-const mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  effect: 'fade',
+document.addEventListener('DOMContentLoaded', function () {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
 });
